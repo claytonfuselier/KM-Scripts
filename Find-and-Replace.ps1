9395d1a81038f5d72f59b1324e1773c26744d3b1
Clayton (Msft Azure) <53104388+claytonfuselier@users.noreply.github.com>
2023-05-20 14:48:43 -0500

    Update Find-and-Replace.ps1

--- Find-and-Replace.ps1
+++ Find-and-Replace.ps1
@@ -1,13 +1,13 @@
 ###############
 ##  Summary  ##
 ###############
-# Intended use is on CodeWiki pages in a locally cloned Azure DevOps (or similar local repositiory).
+# Intended use is on CodeWiki pages in a locally cloned Azure DevOps (or similar local repository).
 # Focus is on performing a simple find & replace to swap every occurrence of one string for another,
 # across every .md file.
 #
-# Note: The $find string is case insentivie.
+# Note: The $find string is case-insensitive.
 # Note: Regex is supported in the $find string. Be sure to escape special characters by using the
-#       backtick/grave acceent (`).
+#       backtick/grave accent (`).
 #
 # Source: https://github.com/claytonfuselier/KM-Scripts/blob/main/Find-and-Replace.ps1
 
@@ -16,7 +16,7 @@
 ##########################
 ##  Required Variables  ##
 ##########################
-$gitroot = ""   # Local cloned repository (ex. "C:\Users\<username>\git\<repo>")
+$gitroot = ""   # Local cloned repository (e.g., "C:\Users\<username>\git\<repo>")
 $find = ""      # String to find and replace. Supports regex (see note above)
 $replace = ""   # New content to replace the old content
 
@@ -28,7 +28,7 @@ $replace = ""   # New content to replace the old content
 $scriptstart = Get-Date
 
 # Get pages
-$pages = Get-ChildItem -Path $gitroot -Recurse | where {$_.Extension -eq ".md"}
+$pages = Get-ChildItem -Path $gitroot -Recurse | where { $_.Extension -eq ".md" }
 
 # Parse Each Page
 $pagecnt = 0
@@ -36,33 +36,33 @@ $totaledits = 0
 $editedpages = 0
 $pages | ForEach-Object {
     # Output current page to host
-    Write-Host -ForegroundColor Gray "$($_.FullName.Replace($gitroot,''))"
+    Write-Host -ForegroundColor Gray ("$($_.FullName.Replace($gitroot,''))")
 
     # Get page content
     $pagecontent = Get-Content -Encoding UTF8 -LiteralPath $_.FullName
 
     # Counting/Checking for matches
-    $matches = ([regex]::Matches($pagecontent, [regex]::Escape($find), [Text.RegularExpressions.RegexOptions]::IgnoreCase)).Count
-    if($matches -gt 0){
+    $matches = ([regex]::Matches($pagecontent, [regex]::Escape($find), [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)).Count
+    if ($matches -gt 0) {
 
         # Find and replace
         $newcontent = $pagecontent -ireplace [regex]::Escape($find), $replace
 
         # Save the new content
-        Set-Content -Encoding UTF8 -Path $_.FullName -Value $newcontent
+        $newcontent | Set-Content -Encoding UTF8 -Path $_.FullName
 
         # Console output plus running tally
-        Write-Host -ForegroundColor Cyan "Updated $matches occurrences"
-        $totaledits = $totaledits + $matches
+        Write-Host -ForegroundColor Cyan "Updated $matches occurrence(s)"
+        $totaledits += $matches
         $editedpages++
     }
 
     # Progress bar
     $pagecnt++
-    $avg = ((Get-Date) – $scriptstart).TotalMilliseconds/$pagecnt
-    $msleft = (($pages.Count–$pagecnt)*$avg)
-    $time = New-TimeSpan –Seconds ($msleft/1000)
-    $percent = [MATH]::Round(($pagecnt/$pages.Count)*100,2)
+    $avg = ((Get-Date) - $scriptstart).TotalMilliseconds / $pagecnt
+    $msleft = (($pages.Count - $pagecnt) * $avg)
+    $time = New-TimeSpan -Seconds ($msleft / 1000)
+    $percent = [Math]::Round(($pagecnt / $pages.Count) * 100, 2)
     Write-Progress -Activity "Scanning pages: $percent %" -Status "$pagecnt of $($pages.Count) total pages - $time" -PercentComplete $percent
 }
 
